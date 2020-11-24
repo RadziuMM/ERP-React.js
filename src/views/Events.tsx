0571f139ab5208d865d1b03caa07e966071f47e3
Radoslaw Mazurczak <RMazurczak@protonmail.com>
2020-11-24 17:45:25 +0100

    events frontend update

--- src/views/Events.tsx
+++ src/views/Events.tsx
@@ -9,6 +9,7 @@ class Events extends React.Component {
     let id: any;
     let data: any[] = [];
     let isEditE: boolean = false;
+    let employers:any[]=[];
 
 
     const addEvent = ()=> {
@@ -22,12 +23,23 @@ class Events extends React.Component {
         args.push(
           (document.getElementById(`ttAdd`)! as HTMLInputElement).value
         );
-        args.push(
-          (document.getElementById(`oAdd`)! as HTMLInputElement).value
-        );
-        args.push(
-          (document.getElementById(`mAdd`)! as HTMLInputElement).value
-        );
+        employers.forEach(element=>{
+          const correct =`${element.firstName} ${element.lastName}`
+          if(correct===(document.getElementById(`oAdd`)! as HTMLInputElement).value){
+            args.push(element.id);
+          }
+        })
+        if(args.length===3){
+          args.push(0);
+        }
+        let members:any = []
+        employers.forEach(element=>{
+          const box = (document.getElementById(`checkMeA${element.id}`)as HTMLInputElement).checked
+          if(box===true){
+            members.push(Number((document.getElementById(`checkMeA${element.id}`)as HTMLInputElement).value))
+          }
+        })
+        args.push(JSON.stringify(members));
         axios
         .post("/api/events/add", {
           acc_id: `${id}`,
@@ -70,12 +82,23 @@ class Events extends React.Component {
         args.push(
           (document.getElementById(`tti${arg0}`)! as HTMLInputElement).value
         );
-        args.push(
-          (document.getElementById(`oi${arg0}`)! as HTMLInputElement).value
-        );
-        args.push(
-          (document.getElementById(`mi${arg0}`)! as HTMLInputElement).value
-        );
+        employers.forEach(element=>{
+          const correct =`${element.firstName} ${element.lastName}`
+          if(correct===(document.getElementById(`oi${arg0}`)! as HTMLInputElement).value){
+            args.push(element.id);
+          }
+        })
+        if(args.length===3){
+          args.push(0);
+        }
+        let members:any = []
+        employers.forEach(element=>{
+          const box = (document.getElementById(`checkMe${element.id}`)as HTMLInputElement).checked
+          if(box===true){
+            members.push(Number((document.getElementById(`checkMe${element.id}`)as HTMLInputElement).value))
+          }
+        })
+        args.push(JSON.stringify(members));
         axios
         .post("/api/events/edit", {
           id: `${arg0}`,
@@ -100,12 +123,19 @@ class Events extends React.Component {
               `editE${Element.id}`
             )! as HTMLButtonElement).disabled = true;
           };
-          console.log(arg3);
+          let preselect =""
+          let list = ''
+          employers.forEach(element=>{
+            list += `<input name="checkM${element.id}" id="checkMe${element.id}" type="checkbox" value="${element.id}"/><label for="checkM${element.id}">${element.firstName} ${element.lastName}</label><br>`
+            if(arg3.owner_id===element.id){
+              preselect=`${element.firstName} ${element.lastName}`
+            }
+          });
         (document.getElementById(`n${arg0}`)! as HTMLTableCellElement).innerHTML = `<input id="ni${arg0}" value="${arg3.name}" />`;
         (document.getElementById(`tf${arg0}`)! as HTMLTableCellElement).innerHTML = `<input id="tfi${arg0}" value="${arg3.time_from}" />`;
         (document.getElementById(`tt${arg0}`)! as HTMLTableCellElement).innerHTML = `<input id="tti${arg0}" value="${arg3.time_to}" />`;
-        (document.getElementById(`o${arg0}`)! as HTMLTableCellElement).innerHTML = `<input id="oi${arg0}" value="${arg3.owner_id}" />`;
-        (document.getElementById(`m${arg0}`)! as HTMLTableCellElement).innerHTML = `<input id="mi${arg0}" value="${arg3.members_ids}" />`;
+        (document.getElementById(`o${arg0}`)! as HTMLTableCellElement).innerHTML = `<input id="oi${arg0}" onclick='document.getElementById("oi${arg0}").value = ""' list="owners" value="${preselect}" />`;
+        (document.getElementById(`m${arg0}`)! as HTMLTableCellElement).innerHTML = `${list}`;
         })
       }
     };
@@ -129,7 +159,7 @@ class Events extends React.Component {
           <td id="tf${Element.id}">${Element.time_from}</td>
           <td id="tt${Element.id}">${Element.time_to}</td>
           <td id="o${Element.id}">${Element.owner_id}</td>
-          <td id="m${Element.id}">${Element.members_ids}</td>
+          <td id="m${Element.id}">${JSON.parse(Element.members_ids)}</td>
           <td><button id="editE${Element.id}">Edit</button></td>
           <td><button id="deleteE${Element.id}">Delete</button></td>
         </tr>`;
@@ -139,8 +169,8 @@ class Events extends React.Component {
           <td><input id="nAdd" /></td>
           <td><input id="tfAdd" /></td>
           <td><input id="ttAdd" /></td>
-          <td><input id="oAdd" /></td>
-          <td><input id="mAdd" /></td>
+          <td><input id="oAdd" list="owners" onclick='document.getElementById("oAdd").value = ""'/></td>
+          <td id="tmAdd"><input id="mAdd" /></td>
           <td><button id="addEvent">Add</button></td>
         </tr>`;
       parent.appendChild(events);
@@ -161,6 +191,46 @@ class Events extends React.Component {
       )! as HTMLButtonElement).onclick = () => {
         addEvent();
       };
+      axios
+      .post("/api/emp/get", {
+        acc_id: `${id}`,
+      })
+      .then((res: { statusCode: any; data: any }) => {
+        employers = res.data
+        data.forEach((element) => {
+          for(let i =0;i<employers.length;i+=1){
+            if (element.owner_id === employers[i].id){
+              (document.getElementById(`o${element.id}`)!as HTMLDivElement).innerHTML = `${employers[i].firstName} ${employers[i].lastName}`;
+            }
+          }
+          let members = '';
+          for(let i = 0;i<JSON.parse(element.members_ids).length;i += 1){
+              for(let j=0;j<employers.length;j+=1){
+                if(employers[j].id===JSON.parse(element.members_ids)[i]){
+                  members += `${employers[j].firstName} ${employers[j].lastName}, `;
+                  (document.getElementById(`m${element.id}`)!as HTMLDivElement).innerHTML = members;
+                }
+              }
+          }
+        })
+        const list = document.getElementById("ownersBlock")! as HTMLDivElement
+        list.innerHTML = ""
+        const option = document.createElement("datalist") as HTMLElement
+        employers.forEach(element=>{
+          option.innerHTML += `<option value="${element.firstName} ${element.lastName}"/>`
+        })
+        option.id = "owners"
+        list?.appendChild(option)
+        let mlist = ''
+        employers.forEach(element=>{
+          mlist += `<input name="checkM${element.id}" id="checkMeA${element.id}" type="checkbox" value="${element.id}"/><label for="checkM${element.id}">${element.firstName} ${element.lastName}</label><br>`
+        });
+        (document.getElementById("tmAdd")as HTMLTableCellElement).innerHTML= mlist
+      })
+      .catch((error: any) => {
+        console.error(error);
+      });
+
     };
     const fetch = () => {
       axios
@@ -194,6 +264,7 @@ class Events extends React.Component {
         </span>
         /Events/
         <div id="Events"></div>
+        <div id="ownersBlock"></div>
       </div>
     );
   }
